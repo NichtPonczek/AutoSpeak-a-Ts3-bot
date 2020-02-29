0c668ea3e6860f7c2330f8b39f0188e995eacd9c
NichtPonczek <mklyza@gmail.com>
2020-02-29 20:35:55 +0200

    Update example.sql

--- example.sql
+++ example.sql
@@ -1,233 +1,178 @@
--- Host: localhost
--- Time: 29 Dec 2019, 11:37
--- Server: 5.7.28-0ubuntu0.16.04.2
--- PHP: 7.0.33-0ubuntu0.16.04.7
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : localhost:3306
+ Source Schema         : autospeak
 
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 29/02/2020 20:34:15
+*/
 
---
--- Db: `AutoSpeak`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for actions
+-- ----------------------------
+DROP TABLE IF EXISTS `actions`;
+CREATE TABLE `actions`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `date` int(0) NOT NULL,
+  `name` longtext CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `text` longtext CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `instance_id` int(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1833 CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `actions` (
-  `id` int(11) NOT NULL,
-  `date` int(11) NOT NULL,
-  `name` longtext COLLATE utf8_polish_ci NOT NULL,
-  `text` longtext COLLATE utf8_polish_ci NOT NULL,
-  `instance_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+-- ----------------------------
+-- Table structure for clients
+-- ----------------------------
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE `clients`  (
+  `client_dbid` bigint(0) NOT NULL,
+  `client_clid` bigint(0) NOT NULL,
+  `client_nick` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `last_nicks` varchar(1024) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `client_uid` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `server_groups` longtext CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `connections` bigint(0) NOT NULL,
+  `connected_time` bigint(0) NOT NULL DEFAULT 0,
+  `connected_time_record` bigint(0) NOT NULL,
+  `idle_time_record` bigint(0) NOT NULL,
+  `time_spent` bigint(0) NOT NULL,
+  `idle_time_spent` bigint(0) NOT NULL,
+  `week_start` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `week_start_time` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `banner_clock` int(0) NOT NULL DEFAULT 1,
+  `banner_additional` int(0) NOT NULL DEFAULT 1,
+  `banner_online` int(0) NOT NULL DEFAULT 1,
+  `banner_admins` int(0) NOT NULL DEFAULT 1,
+  `banner_date` int(0) NOT NULL DEFAULT 1,
+  `banner_weather` int(0) NOT NULL DEFAULT 1,
+  `banner_bg` int(0) NOT NULL DEFAULT 0,
+  `connection_client_ip` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
+  `client_version` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `client_lastconnected` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`client_dbid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for new_daily_users
+-- ----------------------------
+DROP TABLE IF EXISTS `new_daily_users`;
+CREATE TABLE `new_daily_users`  (
+  `client_dbid` int(0) NOT NULL,
+  `client_clid` int(0) NOT NULL,
+  `client_nick` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `client_uid` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `day` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`client_dbid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for random_group
+-- ----------------------------
+DROP TABLE IF EXISTS `random_group`;
+CREATE TABLE `random_group`  (
+  `client_dbid` int(0) NOT NULL,
+  `sgid` int(0) NOT NULL,
+  `date` bigint(0) NOT NULL,
+  `time` bigint(0) NOT NULL,
+  `deleted` int(0) NOT NULL,
+  PRIMARY KEY (`date`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `clients` (
-  `client_dbid` int(255) NOT NULL,
-  `client_clid` int(11) NOT NULL,
-  `client_nick` text COLLATE utf8_polish_ci NOT NULL,
-  `last_nicks` longtext COLLATE utf8_polish_ci NOT NULL,
-  `client_uid` text CHARACTER SET latin1 NOT NULL,
-  `server_groups` longtext COLLATE utf8_polish_ci NOT NULL,
-  `connections` int(11) NOT NULL,
-  `connected_time` int(11) NOT NULL DEFAULT '0',
-  `connected_time_record` bigint(255) NOT NULL,
-  `idle_time_record` bigint(255) NOT NULL,
-  `time_spent` bigint(255) NOT NULL,
-  `idle_time_spent` bigint(255) NOT NULL,
-  `week_start` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  `week_start_time` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  `banner_clock` int(11) NOT NULL DEFAULT '1',
-  `banner_additional` int(11) NOT NULL DEFAULT '1',
-  `banner_online` int(11) NOT NULL DEFAULT '1',
-  `banner_admins` int(11) NOT NULL DEFAULT '1',
-  `banner_date` int(11) NOT NULL DEFAULT '1',
-  `banner_weather` int(11) NOT NULL DEFAULT '1',
-  `banner_bg` int(11) NOT NULL DEFAULT '0',
-  `connection_client_ip` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  `client_version` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  `client_lastconnected` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=DYNAMIC;
+-- ----------------------------
+-- Table structure for statistics
+-- ----------------------------
+DROP TABLE IF EXISTS `statistics`;
+CREATE TABLE `statistics`  (
+  `dbid` int(0) NOT NULL,
+  `groups_day_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `groups_day_normal` int(0) NOT NULL DEFAULT 0,
+  `groups_day_reg` int(0) NOT NULL DEFAULT 0,
+  `groups_week_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `groups_week_normal` int(0) NOT NULL DEFAULT 0,
+  `groups_week_reg` int(0) NOT NULL DEFAULT 0,
+  `groups_month_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `groups_month_normal` int(0) NOT NULL DEFAULT 0,
+  `groups_month_reg` int(0) NOT NULL DEFAULT 0,
+  `groups_total_normal` int(0) NOT NULL DEFAULT 0,
+  `groups_total_reg` int(0) NOT NULL DEFAULT 0,
+  `time_day_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `time_day_time` bigint(0) NOT NULL DEFAULT 0,
+  `time_day_offline` bigint(0) NOT NULL DEFAULT 0,
+  `time_week_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `time_week_time` bigint(0) NOT NULL DEFAULT 0,
+  `time_week_offline` bigint(0) NOT NULL DEFAULT 0,
+  `time_month_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `time_month_time` bigint(0) NOT NULL DEFAULT 0,
+  `time_month_offline` bigint(0) NOT NULL DEFAULT 0,
+  `time_total_time` bigint(0) NOT NULL DEFAULT 0,
+  `time_total_offline` bigint(0) NOT NULL DEFAULT 0,
+  `help_day_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `help_day_time` int(0) NOT NULL DEFAULT 0,
+  `help_day_count` int(0) NOT NULL DEFAULT 0,
+  `help_week_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `help_week_time` int(0) NOT NULL DEFAULT 0,
+  `help_week_count` int(0) NOT NULL DEFAULT 0,
+  `help_month_start` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `help_month_time` int(0) NOT NULL DEFAULT 0,
+  `help_month_count` int(0) NOT NULL DEFAULT 0,
+  `help_total_time` int(0) NOT NULL DEFAULT 0,
+  `help_total_count` int(0) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`dbid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `new_daily_users` (
-  `client_dbid` int(255) NOT NULL,
-  `client_clid` int(11) NOT NULL,
-  `client_nick` text CHARACTER SET latin1 NOT NULL,
-  `client_uid` text CHARACTER SET latin1 NOT NULL,
-  `day` text CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+-- ----------------------------
+-- Table structure for vip_channels
+-- ----------------------------
+DROP TABLE IF EXISTS `vip_channels`;
+CREATE TABLE `vip_channels`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `type_id` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `channel_num` int(0) NOT NULL,
+  `channel_cid` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `spacer_cid` int(0) NOT NULL,
+  `online_from` int(0) NOT NULL,
+  `get_group` int(0) NOT NULL,
+  `owner_dbid` int(0) NOT NULL,
+  `group_id` int(0) NOT NULL,
+  `created` int(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for visitors
+-- ----------------------------
+DROP TABLE IF EXISTS `visitors`;
+CREATE TABLE `visitors`  (
+  `client_dbid` int(0) NOT NULL,
+  `day` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`client_dbid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for yt_channels
+-- ----------------------------
+DROP TABLE IF EXISTS `yt_channels`;
+CREATE TABLE `yt_channels`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `channel_num` int(0) NOT NULL,
+  `channel_cid` int(0) NOT NULL,
+  `spacer_cid` int(0) NOT NULL,
+  `main_info` int(0) NOT NULL,
+  `videos_count` int(0) NOT NULL,
+  `views_count` int(0) NOT NULL,
+  `owner_dbid` int(0) NOT NULL,
+  `created` int(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_polish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `random_group` (
-  `client_dbid` int(11) NOT NULL,
-  `sgid` int(11) NOT NULL,
-  `date` bigint(255) NOT NULL,
-  `time` bigint(20) NOT NULL,
-  `deleted` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
-
-CREATE TABLE `statistics` (
-  `dbid` int(10) NOT NULL,
-  `groups_day_start` text COLLATE utf8_polish_ci NOT NULL,
-  `groups_day_normal` int(11) NOT NULL DEFAULT '0',
-  `groups_day_reg` int(11) NOT NULL DEFAULT '0',
-  `groups_week_start` text COLLATE utf8_polish_ci NOT NULL,
-  `groups_week_normal` int(11) NOT NULL DEFAULT '0',
-  `groups_week_reg` int(11) NOT NULL DEFAULT '0',
-  `groups_month_start` text COLLATE utf8_polish_ci NOT NULL,
-  `groups_month_normal` int(11) NOT NULL DEFAULT '0',
-  `groups_month_reg` int(11) NOT NULL DEFAULT '0',
-  `groups_total_normal` int(11) NOT NULL DEFAULT '0',
-  `groups_total_reg` int(11) NOT NULL DEFAULT '0',
-  `time_day_start` text COLLATE utf8_polish_ci NOT NULL,
-  `time_day_time` bigint(20) NOT NULL DEFAULT '0',
-  `time_day_offline` bigint(20) NOT NULL DEFAULT '0',
-  `time_week_start` text COLLATE utf8_polish_ci NOT NULL,
-  `time_week_time` bigint(20) NOT NULL DEFAULT '0',
-  `time_week_offline` bigint(20) NOT NULL DEFAULT '0',
-  `time_month_start` text COLLATE utf8_polish_ci NOT NULL,
-  `time_month_time` bigint(20) NOT NULL DEFAULT '0',
-  `time_month_offline` bigint(11) NOT NULL DEFAULT '0',
-  `time_total_time` bigint(20) NOT NULL DEFAULT '0',
-  `time_total_offline` bigint(20) NOT NULL DEFAULT '0',
-  `help_day_start` text COLLATE utf8_polish_ci NOT NULL,
-  `help_day_time` int(11) NOT NULL DEFAULT '0',
-  `help_day_count` int(11) NOT NULL DEFAULT '0',
-  `help_week_start` text COLLATE utf8_polish_ci NOT NULL,
-  `help_week_time` int(11) NOT NULL DEFAULT '0',
-  `help_week_count` int(11) NOT NULL DEFAULT '0',
-  `help_month_start` text COLLATE utf8_polish_ci NOT NULL,
-  `help_month_time` int(11) NOT NULL DEFAULT '0',
-  `help_month_count` int(11) NOT NULL DEFAULT '0',
-  `help_total_time` int(11) NOT NULL DEFAULT '0',
-  `help_total_count` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
-CREATE TABLE `vip_channels` (
-  `id` int(11) NOT NULL,
-  `type_id` text COLLATE utf8_polish_ci NOT NULL,
-  `channel_num` int(11) NOT NULL,
-  `channel_cid` text COLLATE utf8_polish_ci NOT NULL,
-  `spacer_cid` int(11) NOT NULL,
-  `online_from` int(11) NOT NULL,
-  `get_group` int(11) NOT NULL,
-  `owner_dbid` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
-
-INSERT INTO `vip_channels` (`id`, `type_id`, `channel_num`, `channel_cid`, `spacer_cid`, `online_from`, `get_group`, `owner_dbid`, `group_id`, `created`) VALUES
-(1, 'Diamond', 1, '6', 7, 9, 10, 2, 10, 1573919864),
-(2, 'VIP', 1, '22', 23, 25, 26, 2, 11, 1573920010);
-
--- --------------------------------------------------------
-
-
-CREATE TABLE `visitors` (
-  `client_dbid` int(255) NOT NULL,
-  `day` text COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
-CREATE TABLE `yt_channels` (
-  `id` int(11) NOT NULL,
-  `channel_num` int(11) NOT NULL,
-  `channel_cid` int(11) NOT NULL,
-  `spacer_cid` int(11) NOT NULL,
-  `main_info` int(11) NOT NULL,
-  `videos_count` int(11) NOT NULL,
-  `views_count` int(11) NOT NULL,
-  `owner_dbid` int(11) NOT NULL,
-  `created` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
---
--- Index
---
-
---
--- Indexes for table `actions`
---
-ALTER TABLE `actions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`client_dbid`);
-
---
--- Indexes for table `new_daily_users`
---
-ALTER TABLE `new_daily_users`
-  ADD PRIMARY KEY (`client_dbid`);
-
---
--- Indexes for table `random_group`
---
-ALTER TABLE `random_group`
-  ADD PRIMARY KEY (`date`);
-
---
--- Indexes for table `statistics`
---
-ALTER TABLE `statistics`
-  ADD PRIMARY KEY (`dbid`);
-
---
--- Indexes for table `vip_channels`
---
-ALTER TABLE `vip_channels`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `visitors`
---
-ALTER TABLE `visitors`
-  ADD PRIMARY KEY (`client_dbid`);
-
---
--- Indexes for table `yt_channels`
---
-ALTER TABLE `yt_channels`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT dla tabeli `actions`
---
-ALTER TABLE `actions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1406;
---
--- AUTO_INCREMENT dla tabeli `vip_channels`
---
-ALTER TABLE `vip_channels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT dla tabeli `yt_channels`
---
-ALTER TABLE `yt_channels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;

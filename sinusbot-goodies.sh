a87d16ef6c4823424f858625c439696f9884cb0e
NichtPonczek <mklyza@gmail.com>
2020-03-05 20:57:18 +0200

    Update sinusbot-goodies.sh

--- sinusbot-goodies.sh
+++ sinusbot-goodies.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 apt update
 apt install -y x11vnc xvfb libxcursor1 ca-certificates bzip2 libnss3 libegl1-mesa x11-xkb-utils libasound2 libpci3 libxslt1.1 libxkbcommon0 libxss1 curl
 update-ca-certificates
@@ -9,6 +10,8 @@ VERSION="3.3.2"
 wget https://files.teamspeak-services.com/releases/client/$VERSION/TeamSpeak3-Client-linux_amd64-$VERSION.run
 chmod 0755 TeamSpeak3-Client-linux_amd64-$VERSION.run
 ./TeamSpeak3-Client-linux_amd64-$VERSION.run
+wget https://www.sinusbot.com/dl/sinusbot.current.tar.bz2
+tar -xjf sinusbot.current.tar.bz2
 rm TeamSpeak3-Client-linux_amd64/xcbglintegrations/libqxcb-glx-integration.so
 mkdir TeamSpeak3-Client-linux_amd64/plugins
 cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins/
@@ -20,31 +23,30 @@ mkdir bot2
 mkdir bot3
 mkdir bot4
 cd /opt/bot1
-wget https://www.sinusbot.com/dl/sinusbot.current.tar.bz2
-cp sinusbot.current.tar.bz2 /opt/bot2/sinusbot.current.tar.bz2
-cp sinusbot.current.tar.bz2 /opt/bot3/sinusbot.current.tar.bz2
-cp sinusbot.current.tar.bz2 /opt/bot4/sinusbot.current.tar.bz2
+cp /opt/sinusbot/sinusbot.current.tar.bz2 /opt/bot2/sinusbot.current.tar.bz2
+cp /opt/sinusbot/sinusbot.current.tar.bz2 /opt/bot3/sinusbot.current.tar.bz2
+cp /opt/sinusbot/sinusbot.current.tar.bz2 /opt/bot4/sinusbot.current.tar.bz2
 tar -xjf sinusbot.current.tar.bz2
 echo TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\" > config.ini
-echo ListenPort = 8090 >> config.ini 
+echo ListenPort = 8090 >> config.ini
 echo YoutubeDLPath = \"/usr/local/bin/youtube-dl\" >> config.ini
 chmod 755 sinusbot
 cd /opt/bot2
 tar -xjf sinusbot.current.tar.bz2
 echo TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\" > config.ini
-echo ListenPort = 8091 >> config.ini 
+echo ListenPort = 8091 >> config.ini
 echo YoutubeDLPath = \"/usr/local/bin/youtube-dl\" >> config.ini
 chmod 755 sinusbot
 cd /opt/bot3
 tar -xjf sinusbot.current.tar.bz2
 echo TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\" > config.ini
-echo ListenPort = 8092 >> config.ini 
+echo ListenPort = 8092 >> config.ini
 echo YoutubeDLPath = \"/usr/local/bin/youtube-dl\" >> config.ini
 chmod 755 sinusbot
 cd /opt/bot4
 tar -xjf sinusbot.current.tar.bz2
 echo TS3Path = \"/opt/sinusbot/TeamSpeak3-Client-linux_amd64/ts3client_linux_amd64\" > config.ini
-echo ListenPort = 8093 >> config.ini 
+echo ListenPort = 8093 >> config.ini
 echo YoutubeDLPath = \"/usr/local/bin/youtube-dl\" >> config.ini
 chmod 755 sinusbot
 chown -R sinusbot:sinusbot /opt/bot1

16d4e47958064dd2c2673468d16427a765bb5cbe
NichtPonczek <admin@ponczek.tk>
2020-06-14 17:51:50 +0300

    Update change_lang.sh

--- change_lang.sh
+++ change_lang.sh
@@ -4,17 +4,23 @@ case "$Arsch" in
   "pl") echo 'ustawiono polski?'
 rm include/language_file/eng.txt
 rm include/language_file/de.txt
+rm include/congigs/language.php
 echo true >> include/language_file/pl.txt
+echo '<?php $config['bot_language'] = 'pl'; ?>' >> include/congigs/language.php
  ;;
   "eng") echo 'language set to english!'
 rm include/language_file/pl.txt
 rm include/language_file/de.txt
+rm include/congigs/language.php
 echo true >> include/language_file/eng.txt
+echo '<?php $config['bot_language'] = 'eng'; ?>' >> include/congigs/language.php
 ;;
 "de") echo 'Sprache auf Deutsch eingestelt ...'
 rm include/language_file/pl.txt
 rm include/language_file/de.txt
+rm include/congigs/language.php
 echo true >> include/language_file/de.txt
+echo '<?php $config['bot_language'] = 'de'; ?>' >> include/congigs/language.php
 ;;
   *) echo "no such language! valid languages are de=deutsch eng=english pl=polski"
 esac
